cd2dbd2bd3a3327c586c927b8d841cbd3bdd84f4
Strasseneck <121754696+Strasseneck@users.noreply.github.com>
2023-11-14 09:45:27 +0100

    Add close new post button, add all posts display with basic html

--- network/static/network/script.js
+++ network/static/network/script.js
@@ -5,3 +5,8 @@ $('#create-new-post').on('click', function() {
     console.log('here');
     $('#new-post-form').attr("style", "display: block");
 })
+
+// Close new post button
+$('#close-button').on('click', function () {
+    $('#new-post-form').attr("style", "display: none");
+})
