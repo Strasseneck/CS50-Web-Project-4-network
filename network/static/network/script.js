a8e9e1571a57bbc2096d084fb6ad3bc3e857c773
Strasseneck <121754696+Strasseneck@users.noreply.github.com>
2023-11-15 17:05:28 +0100

    Add save post button function

--- network/static/network/script.js
+++ network/static/network/script.js
@@ -16,29 +16,59 @@ $('#close-button').on('click', function () {
 $('#edit-post-button').on('click', function () {
 
     console.log('edit post clicked')
-    // Get the postid
-    let postid = $('#post-id').val();
+    // Get the post body id
+    let postBodyId = $('#post-body-id').val();
+
+    console.log(postBodyId);
+
+    // Get post id
+    let postId = $('#post-id').val();
 
     // Get the posts body content
-    let $postBody = $(`#${postid}`);
+    let $postBody = $(`#${postBodyId}`);
     let postBodyText = $postBody.text();
+
+    console.log(postBodyText);
 
     // Create post body text area and populate with content
     let $editableBody = $('<textarea>');
     $editableBody.addClass('form-control');
-    $editableBody.attr('name', 'new-body');
+    $editableBody.attr("id", 'new-body');
     $editableBody.val(postBodyText);
 
     // Replace the original body with the new editable body
     $postBody.replaceWith($editableBody);
+    
+    // Get edit button
+    let $editButton = $('#edit-post-button');
 
     // Replace edit post button with save post button
     let $saveButton = $('<input>');
     $saveButton.addClass('btn btn-primary btn-sm');
     $saveButton.attr('type', 'button');
     $saveButton.val('Save Post');
+   
+   
+    $saveButton.on('click', function () {
 
-    let $editButton = $('#edit-post-button');
+        // Get new body content
+        let newBodyContent = $('#new-body').val();
+        
+        // Replace text area with body again
+        $postBody.text(newBodyContent);
+        $editableBody.replaceWith($postBody);
+
+        // Replace save button with edit button
+        savePost(postId);
+    })
+    
     $editButton.replaceWith($saveButton);
 })
 
+// Save post function
+
+function savePost(postId) {
+    console.log('Save Post');
+    console.log(postId);
+
+}
