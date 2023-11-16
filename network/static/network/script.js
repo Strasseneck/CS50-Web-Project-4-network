89f61df95e1bdef5c07317ffa4a426eeec84e7e2
Strasseneck <121754696+Strasseneck@users.noreply.github.com>
2023-11-16 11:22:39 +0100

    Add improved button behaviour and functionality

--- network/static/network/script.js
+++ network/static/network/script.js
@@ -16,6 +16,14 @@ $('#close-button').on('click', function () {
 // Show button on hover
 $('.post-container').on({
     mouseenter: function () {
+        // Check if savebutton exists
+        let $saveButton = $('#save-post-button');
+        if($saveButton.length !== 0) {
+            // If exists return
+            return;
+        }
+
+        // Get post, user data
         console.log('mouse enter');
         let postId = $(this).attr('id');
         let postAuthor = $(this).find('#user-profile').text();
@@ -28,7 +36,9 @@ $('.post-container').on({
             editButton.attr('type', 'button');
             editButton.attr('id', 'edit-post-button');
             editButton.val('Edit Post');
-            editButton.on('click', editPost(postId));
+            editButton.on('click', function () {
+                editPost(postId)
+            });
             editButton.appendTo(`#button-container${postId}`)
         }
     },
@@ -45,10 +55,41 @@ $('.post-container').on({
 // Edit post function
 function editPost(postId) {
     console.log('edit post clicked')
+    // Get the post to edit
+    let $post = $(`#${postId}`);
+
+    $post.each(function() {
+        // Get the post body
+        let $postBody = $(this).find('#post-body');
+        let postBodyText = $postBody.text();
+        console.log(postBodyText);
+
+        // Create post body text area and populate with content
+        let $editableBody = $('<textarea>');
+        $editableBody.addClass('form-control');
+        $editableBody.attr("id", 'new-body');
+        $editableBody.val(postBodyText);
+
+        // Replace the original body with the new editable body
+        $postBody.replaceWith($editableBody);
+
+        // Get edit button
+        let $editButton = $(this).find('#edit-post-button');
+
+        // Replace edit post button with save post button
+        let $saveButton = $('<input>');
+        $saveButton.addClass('btn btn-primary btn-sm');
+        $saveButton.attr('type', 'button');
+        $saveButton.attr('id', 'save-post-button')
+        $saveButton.val('Save Post');
+        $saveButton.on('click', function () {
+            savePost($post)
+        });
+        $editButton.replaceWith($saveButton);  
+    })
 }
 
 // Save post function
-function savePost(postId) {
+function savePost(post) {
     console.log('Save Post');
-    console.log(postId);
 }
